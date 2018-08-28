845e34662a83a2f0e9013cc2be2fc63b5892ba94
DiegoRLima <aranhajba@gmail.com>
2018-08-27 22:35:39 -0300

    Initial commit

---
+++ Assets/Scripts/Forca.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Forca : MonoBehaviour {
+
+    private Rigidbody2D bola;
+    public float force = 0f;
+    private Rotacao rota;
+    public Image seta2Image;
+
+    // Use this for initialization
+    void Start() {
+        bola = GetComponent<Rigidbody2D>();
+        rota = GetComponent<Rotacao>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ControlaForca();
+        AplicaForca();
+    }
+
+    void AplicaForca()
+    {
+        //if (Input.GetKeyUp(KeyCode.Space))
+        if (rota.liberaTiro == true)
+        {
+            float x = force * Mathf.Cos(rota.zRotate * Mathf.Deg2Rad);
+            float y = force * Mathf.Sin(rota.zRotate * Mathf.Deg2Rad);
+            bola.AddForce(new Vector2(x, y));
+
+        }
+    }
+
+    void ControlaForca()
+    {
+        if (rota.liberaRota == true)
+        {
+            float moveX = Input.GetAxis("Mouse X");
+
+            if (moveX < 0)
+            {
+                seta2Image.fillAmount += 0.8f * Time.deltaTime;
+                force = seta2Image.fillAmount * 10;
+            }
+
+            if (moveX > 0)
+            {
+                seta2Image.fillAmount -= 0.8f  * Time.deltaTime;
+                force = seta2Image.fillAmount * 10;
+            }
+        }
+    }
+}
+//String[] substrings = value.Split(delimiter);
+
+
+//using System;
+
+//public class Test
+//{
+//    public static void Main()
+//    {
+//        string line;
+//        while ((line = Console.ReadLine()) != null)
+//        {
+//            int n1, dividendo;
+//            int n2, divisor, resto;
+
+//            n1 = 2;
+//            n2 = 4;
+
+//            if (n1 > n2)
+//            {
+//                dividendo = n1;
+//                divisor = n2;
+//            }
+//            else
+//            {
+//                dividendo = n2;
+//                divisor = n1;
+//            }
+
+//            while (dividendo % divisor != 0)
+//            {
+//                resto = dividendo % divisor;
+//                dividendo = divisor;
+//                divisor = resto;
+//            }
+
+//            Console.WriteLine(line);
+//        }
+//    }
+//}
---
+++ Assets/Scripts/Rotacao.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rotacao : MonoBehaviour {
+
+    //Posição Seta
+    [SerializeField] private Transform posStart;
+    //Seta
+    [SerializeField] private Image setaImg;
+
+    public GameObject setaGO;
+
+    //Ang
+    public float zRotate;
+    public bool liberaRota = false;
+    public bool liberaTiro = false;
+
+    // Use this for initialization
+    void Start () {
+        PosicionaSeta();
+        PosicionaBola();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RotacaoSeta();
+        InputDeRotacao();
+        LimitaRotacao();
+	}
+
+    void PosicionaSeta()
+    {
+        setaImg.rectTransform.position = posStart.position + new Vector3(0.5f, 0, zRotate); ;
+    }
+
+    void PosicionaBola()
+    {
+        this.gameObject.transform.position = posStart.position;
+    }
+
+    void RotacaoSeta()
+    {
+        setaImg.rectTransform.eulerAngles = new Vector3(0, 0, zRotate);
+    }
+
+    void InputDeRotacao()
+    {
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    zRotate += 2.5f;
+        //}
+
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    zRotate -= 2.5f;
+        //}
+
+        if (liberaRota == true) 
+        {
+            //float moveX = Input.GetAxis("Mouse X");
+            float moveY = Input.GetAxis("Mouse Y");
+
+            if(zRotate < 90)
+            {
+                if (moveY > 0)
+                {
+                    zRotate += 2.5f;
+                }
+            }
+
+            if (zRotate > 0)
+            {
+                if (moveY < 0)
+                {
+                    zRotate -= 2.5f;
+                }
+            }
+        }
+    }
+
+    void LimitaRotacao()
+    {
+        if (zRotate >= 90)
+        {
+            zRotate = 90;
+        }
+
+        if (zRotate <= 0)
+        {
+            zRotate = 0;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        liberaRota = true;
+        //liberaTiro = false;
+        setaGO.SetActive(true);
+    }
+
+    private void OnMouseUp()
+    {
+        liberaRota = false;
+        liberaTiro = true;
+        setaGO.SetActive(false);
+    }
+}
